ec0e1c50d3c411300247d3e23163b0076cc0c6a1
nothowstorygoes <esposito.pioalessandro@gmail.com>
2024-08-08 16:10:32 +0200

    ❗: Reworked navbar, user now can upload profile pictures

--- src/app/components/navbar/navbar.jsx
+++ src/app/components/navbar/navbar.jsx
@@ -1,16 +1,72 @@
 import React from "react";
-import styles from './navbar.module.css'
+import styles from "./navbar.module.css";
+import { onAuthStateChanged } from "firebase/auth";
+import { auth, storage } from "../../firebase";
+import { useState, useEffect } from "react";
+import { getDownloadURL, ref } from "firebase/storage";
 
 export default function Navbar() {
-    return(
+  const [user, setUser] = useState(null);
+  const [propic, setPropic] = useState("");
 
-        <div className={styles.navbar}>
-            <div className={styles.navbarContainer}>
-                <a href="/gymbuddy/dashBoard" className={styles.navbarButton}><img src="/gymbuddy/dashboard.png" className={styles.navbarImg}/><p>dashboard</p></a>
-                <a href="/gymbuddy/workout" className={styles.navbarButton}><img src="/gymbuddy/logWorkout.png"className={styles.navbarImg}/><p>work out</p></a>
-                <a href="/gymbuddy/protein" className={styles.navbarButton}><img src="/gymbuddy/logProtein.png"className={styles.navbarImg}/><p>log</p></a>
-                <a href="/gymbuddy/profile" className={styles.navbarButton}><img src="/gymbuddy/profile.png"className={styles.navbarImg}/><p>profile</p></a>
-            </div>
-            </div>
-    )
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      if (user) {
+        setUser(user);
+        const infoRef = ref(storage, `${user.uid}/info.json`);
+        getDownloadURL(infoRef)
+          .then((url) => fetch(url))
+          .then((response) => response.json())
+          .then((data) => {
+            console.log(data.propic);
+            if (data.propic === true) {
+              const picRef = ref(storage, `${user.uid}/proPic.png`);
+              getDownloadURL(picRef).then((url) => {
+                fetch(url)
+                  .then((res) => res.blob())
+                  .then((blob) => {
+                    const img = URL.createObjectURL(blob);
+                    console.log(img);
+                    localStorage.setItem("propic", img); // Cache the image URL
+                    setPropic(img);
+                  });
+              });
+            } else {
+              setPropic("/gymbuddy/profile.png");
+            }
+          });
+      }
+    });
+
+    // Check if the image is already cached
+    const cachedPropic = localStorage.getItem("propic");
+    if (cachedPropic) {
+      setPropic(cachedPropic);
+    }
+
+    return () => unsubscribe();
+  }, []);
+
+  return (
+    <div className={styles.navbar}>
+      <div className={styles.navbarContainer}>
+        <a href="/gymbuddy/dashBoard" className={styles.navbarButton}>
+          <img src="/gymbuddy/dashboard.png" className={styles.navbarImg} loading="lazy" />
+          <p>dashboard</p>
+        </a>
+        <a href="/gymbuddy/workout" className={styles.navbarButton}>
+          <img src="/gymbuddy/logWorkout.png" className={styles.navbarImg} loading="lazy" />
+          <p>work out</p>
+        </a>
+        <a href="/gymbuddy/protein" className={styles.navbarButton}>
+          <img src="/gymbuddy/logProtein.png" className={styles.navbarImg} loading="lazy" />
+          <p>log</p>
+        </a>
+        <a href="/gymbuddy/profile" className={styles.navbarButton}>
+          <img src={propic} className={styles.navbarPropic} loading="lazy" />
+          <p>profile</p>
+        </a>
+      </div>
+    </div>
+  );
 }
--- src/app/profile/page.jsx
+++ src/app/profile/page.jsx
@@ -1,5 +1,137 @@
+"use client";
 import React from "react";
+import { onAuthStateChanged } from "firebase/auth";
+import { auth, storage } from "../firebase";
+import { useState, useEffect } from "react";
+import { uploadBytesResumable } from "firebase/storage";
+import { getDownloadURL, ref, uploadString } from "firebase/storage";
+import styles from "./profile.module.css";
+import Navbar from "../components/navbar/navbar";
+import LoadingSpinner from "../components/loadingSpinner/loadingSpinner";
 
 export default function Profile() {
-    return <div>Ehi. fsasds</div>;
+  const [user, setUser] = useState(null);
+  const [info, setInfo] = useState([]);
+  const [tempPropic, setTempPropic] = useState("");
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      if (user) {
+        setUser(user);
+        // Download info.json
+        const infoRef = ref(storage, `${user.uid}/info.json`);
+        getDownloadURL(infoRef)
+          .then((url) => fetch(url))
+          .then((response) => response.json())
+          .then((data) => {
+            setInfo(data);
+            console.log(data);
+          })
+          .catch((error) => console.error("Error fetching info.json:", error));
+      }
+      if (info.propic === null) {
+        setInfo((prevInfo) => ({ ...prevInfo, propic: "" }));
+        setTempPropic("/gymbuddy/profile.png");
+        setTimeout(() => {
+          setLoading(false);
+        }, 500);
+      } else {
+        getDownloadURL(ref(storage, `${user.uid}/proPic.png`)).then((url) => {
+          fetch(url)
+            .then((res) => res.blob())
+            .then((blob) => {
+              const img = URL.createObjectURL(blob);
+              setTempPropic(img);
+              setTimeout(() => {
+                setLoading(false);
+              }, 500);
+            });
+        });
+      }
+    });
+  }, []);
+
+  useEffect(() => {
+    console.log(info);
+  }, [info]);
+
+  const handlePropicEdit = () => {
+    if (info.propic === undefined) {
+      setInfo((prevInfo) => ({ ...prevInfo, propic: "" }));
     }
+    console.log("im in");
+    const input = document.createElement("input");
+    input.type = "file";
+    input.accept = "image/*";
+    input.onchange = (e) => {
+      const file = e.target.files[0];
+      const proPicRef = ref(storage, `${user.uid}/proPic.png`);
+      setTempPropic(URL.createObjectURL(file));
+      const uploadTask = uploadBytesResumable(proPicRef, file);
+      uploadTask.on(
+        "state_changed",
+        (snapshot) => {
+          const progress =
+            (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+          console.log(`Upload is ${progress}% done`);
+          switch (snapshot.state) {
+            case "paused":
+              console.log("Upload is paused");
+              break;
+            case "running":
+              console.log("Upload is running");
+              break;
+          }
+        },
+        (error) => {
+          console.error(error);
+        },
+        () => {
+          getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+            console.log("File available at", downloadURL);
+            setTempPropic(downloadURL);
+          });
+        }
+      );
+      setInfo((prevInfo) => ({ ...prevInfo, propic: true }));
+      const infoRef = ref(storage, `${user.uid}/info.json`);
+      uploadString(infoRef, JSON.stringify({ ...info, propic: true }));
+    };
+    input.click(); // Trigger the file input click
+  };
+
+  return (
+    <main className={styles.mainContainer}>
+      {loading ? (
+        <LoadingSpinner />
+      ) : (
+        <div className={styles.mainMainContainer}>
+          <div className={styles.greetingContainer}>
+            <p className={styles.greeting}>Good to see you, </p>
+            <p className={styles.name}> &nbsp;{info.username}</p>
+          </div>
+          <div className={styles.propicContainer}>
+            <img
+              src={tempPropic}
+              alt="Profile Picture"
+              className={styles.propic}
+            />
+            <div className={styles.editPropic} onClick={handlePropicEdit}>
+              <img
+                src="/gymbuddy/edit.png"
+                alt="Edit Profile Picture"
+                className={styles.editPropicImg}
+              />
+            </div>
+            <div className={styles.infoContainer}>
+                
+                </div>
+          </div>
+          <Navbar />
+        </div>
+        
+      )}
+    </main>
+  );
+}
