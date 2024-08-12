24e2ddae3425486da83c17e538d22772a94608e1
nothowstorygoes <esposito.pioalessandro@gmail.com>
2024-08-12 14:52:04 +0200

    chore: add logging

--- src/app/logFood/fetchProduct/page.jsx
+++ src/app/logFood/fetchProduct/page.jsx
@@ -23,12 +23,15 @@ function ProductFetcher() {
     return () => clearTimeout(delayDebounceFn);
   }, [productName]);
 
+  const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
+
   async function searchProducts() {
     setLoading(true);
     setError(null);
 
     const userAgent = "gymBuddy/1.2 (pioesposito2003@gmail.com)";
     try {
+      await delay(500); // Delay each request by 500ms
       const response = await fetch(
         `https://world.openfoodfacts.org/cgi/search.pl?search_terms=${productName}&search_simple=1&action=process&json=1`,
         {
@@ -46,10 +49,10 @@ function ProductFetcher() {
     }
   }
 
-  const pushWithParams = ( id ) => { 
+  const pushWithParams = (id) => {
     const params = new URLSearchParams({ id: id });
     router.push(`/logFood/fetchProduct/productDetails?${params.toString()}`);
-};
+  };
 
   return (
     <main className={styles.mainContainer}>
@@ -62,22 +65,27 @@ function ProductFetcher() {
       />
 
       {loading && <p>Loading...</p>}
-      {error && <p className={styles.error}>Error: {error}</p>}
+      {error && <p className={styles.error}>Error: {error.message}</p>}
 
       {!loading && products.length > 0 && (
         <div className={styles.productList}>
           {products
-      .filter(
-        (product) =>
-          product.nutriments.carbohydrates_100g !== undefined &&
-          product.nutriments.fat_100g !== undefined &&
-          product.nutriments.proteins_100g !== undefined
-      )
-      .map((product) => (
-        <div key={product.id} className={styles.product}>
-          <p onClick={() => (pushWithParams(product._id))} className={styles.productName}>{product.product_name}</p>
-        </div>
-      ))}
+            .filter(
+              (product) =>
+                product.nutriments.carbohydrates_100g !== undefined &&
+                product.nutriments.fat_100g !== undefined &&
+                product.nutriments.proteins_100g !== undefined
+            )
+            .map((product) => (
+              <div key={product.id} className={styles.product}>
+                <p
+                  onClick={() => pushWithParams(product._id)}
+                  className={styles.productName}
+                >
+                  {product.product_name}
+                </p>
+              </div>
+            ))}
         </div>
       )}
       <Navbar />
