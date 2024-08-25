e493b409070966272e319185cb9d032d593c5efe
nothowstorygoes <esposito.pioalessandro@gmail.com>
2024-08-25 15:53:44 +0200

    try to offline 2

--- next.config.mjs
+++ next.config.mjs
@@ -2,25 +2,41 @@ import withPWAInit from "@ducanh2912/next-pwa";
 
 const withPWA = withPWAInit({
   dest: "public",
-  register:true,
+  register: true,
   fallbacks: {
     document: '/~offline', // Fallback for all page requests
   },
-});
-      
-export default withPWA({
-
-    webpack: (config) => {
-      config.resolve.alias.canvas = false;
-   
-      return config;
+  runtimeCaching: [
+    {
+      urlPattern: /.*/i,
+      handler: 'NetworkOnly',
+      options: {
+        cacheName: 'network-only',
+        networkTimeoutSeconds: 10,
+        plugins: [
+          {
+            cacheWillUpdate: async ({ request, response, event }) => {
+              if (!response || response.status !== 200) {
+                return caches.match('/~offline');
+              }
+              return response;
+            },
+          },
+        ],
+      },
     },
+  ],
+});
 
+export default withPWA({
+  webpack: (config) => {
+    config.resolve.alias.canvas = false;
+    return config;
+  },
   output: "export",
-  basePath:"/gymbuddy",
+  basePath: "/gymbuddy",
   assetPrefix: "/gymbuddy/",
   images: {
     unoptimized: true,
   },
-
 });
